81c74964c80c782791e19348b2420bcb7fd184f4
Paul Zabelin <paulz@users.noreply.github.com>
2023-02-12 13:06:07 -0800

    test success cases

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,24 @@ fn hello_world() -> String {
     "Hello, world!".to_string()
 }
 
-fn hex_to_rgb(hex: &str) -> (u8, u8, u8) {
+fn hex_to_rgb(hex: &str) -> Result<(u8, u8, u8), &str> {
     let hex = hex.trim_start_matches('#');
 
-    let (r, g, b) = if hex.len() == 6 {
+    let result:Result<(u8, u8, u8), &str> = if hex.len() == 6 {
         let r = u8::from_str_radix(&hex[0..2], 16).unwrap();
         let g = u8::from_str_radix(&hex[2..4], 16).unwrap();
         let b = u8::from_str_radix(&hex[4..6], 16).unwrap();
-        (r, g, b)
+        Ok((r, g, b))
     } else if hex.len() == 3 {
         let r = u8::from_str_radix(&hex[0..1], 16).unwrap();
         let g = u8::from_str_radix(&hex[1..2], 16).unwrap();
         let b = u8::from_str_radix(&hex[2..3], 16).unwrap();
-        (r * 16 + r, g * 16 + g, b * 16 + b)
+        Ok((r * 16 + r, g * 16 + g, b * 16 + b))
     } else {
-        (0, 0, 0)
+        Err("invalid color")
     };
 
-    (r, g, b)
+    result
 }
 
 fn main() {
@@ -53,7 +53,8 @@ fn test_hex_to_rgb() {
     for case in cases.iter() {
         let (hex, expected) = case;
         let result = hex_to_rgb(hex);
-        assert_eq!(result, *expected, "hex: {}", hex);
+        
+        assert_eq!(result.ok().unwrap(), *expected, "hex: {}", hex);
     }
 }
 
